4e14d1a1fd3917b639746dd00e914603c6fea0e9
rg3915 <rg3915@yahoo.com.br>
2015-10-10 22:10:08 -0300

    no django-extensions

--- orcamentos_project/settings.py
+++ orcamentos_project/settings.py
@@ -18,7 +18,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'bootstrapform',
     'widget_tweaks',
     'aggregate_if',
