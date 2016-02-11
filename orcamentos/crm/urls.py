e394f13b5df0ed3ad2f36e6afb07e67e9be2e80c
rg3915 <rg3915@yahoo.com.br>
2016-02-11 02:40:25 -0200

    add form resolved

--- orcamentos/crm/urls.py
+++ orcamentos/crm/urls.py
@@ -4,9 +4,9 @@ from orcamentos.crm import views as c
 
 person_patterns = [
     url(r'^$', c.person_list, name='person_list'),
+    url(r'^add/$', c.person_create, name='person_add'),
     url(r'^(?P<slug>[\w-]+)/$', c.person_detail, name='person_detail'),
     # url(r'^(?P<pk>\d+)/edit/$', c.PersonUpdate.as_view(), name='person_edit'),
-    url(r'^add/$', c.person_create, name='person_add'),
 ]
 
 customer_patterns = [
