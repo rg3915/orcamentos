41bc4de641c63e266be019005b313b7fb37bbb57
rg3915 <rg3915@yahoo.com.br>
2016-03-26 15:50:31 -0300

    shell occupation

--- shell/shell_occupation.py
+++ shell/shell_occupation.py
@@ -1,5 +1,5 @@
 from orcamentos.crm.models import Occupation
-from orcamentos.utils.lists import CUSTOMER_TYPE, COMPANY_LIST, OCCUPATION_LIST
+from orcamentos.utils.lists import OCCUPATION_LIST
 
 obj = [Occupation(occupation=val) for val in OCCUPATION_LIST]
 Occupation.objects.bulk_create(obj)
