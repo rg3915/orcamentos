d8c342d9735df7067f9a077cabf670effd78b284
rg3915 <rg3915@yahoo.com.br>
2016-03-25 22:20:06 -0300

    django-debug-toolbar

--- orcamentos/settings.py
+++ orcamentos/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # thirty apps
     'django_extensions',
+    'debug_toolbar',
     'bootstrapform',
     'widget_tweaks',
     'daterange_filter',
