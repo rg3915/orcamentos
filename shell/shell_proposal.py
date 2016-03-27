0da483b5de20d03412572ce1bc03fedd4bdb7cfd
rg3915 <rg3915@yahoo.com.br>
2016-03-27 02:14:38 -0300

    shell_proposal

--- shell/shell_proposal.py
+++ shell/shell_proposal.py
@@ -1,10 +1,12 @@
-from random import random, randint
-from orcamentos.core.models import Proposal, Category, Work, Person, Employee, Seller, NumLastProposal
-from shell.gen_random_values import gen_string, gen_date, gen_decimal
+from random import randint, choice
+from django.utils import timezone
+from orcamentos.crm.models import Person, Employee, Seller
+from orcamentos.proposal.models import Proposal, Work, NumLastProposal
+from orcamentos.utils.gen_random_values import gen_string, gen_decimal
 
-status_list = ('c', 'elab', 'p', 'co', 'a')
+status_list = ('c', 'elab', 'p', 'co')
 
-REPEAT = 100
+REPEAT = 60
 
 # Return min id of work
 try:
@@ -20,29 +22,31 @@ except Work.DoesNotExist:
 
 for i in range(1, REPEAT + 1):
     c = randint(1, 8)
-    category = Category.objects.get(pk=c)
     description = gen_string(30)
     w = randint(min_work_pk, max_work_pk)
     work = Work.objects.get(pk=w)
-    p = randint(1, 50)
+    # obtem todos os pk de contatos
+    person_pks = [pk[0] for pk in Person.objects.all().values_list('pk')]
+    p = choice(person_pks)
     person = Person.objects.get(pk=p)
-    e = randint(1, 9)
+    # obtem todos os pk de funcionários
+    employee_pks = [pk[0] for pk in Employee.objects.all().values_list('pk')]
+    e = choice(employee_pks)
     employee = Employee.objects.get(pk=e)
-    s = randint(1, 3)
+    # obtem todos os pk de vendedores
+    seller_pks = [pk[0] for pk in Seller.objects.all().values_list('pk')]
+    s = choice(seller_pks)
     seller = Seller.objects.get(pk=s)
-    sl = randint(0, 4)
-    status = status_list[sl]
-    if status == 'co' or status == 'a':
-        date_conclusion = gen_date(min_year=2015, max_year=2015)
+    # escolhe um status
+    status = choice(status_list)
+    if status == 'co':
+        date_conclusion = timezone.now()
         price = gen_decimal(9, 2)
     else:
         date_conclusion = None
         price = 0
     obj = Proposal(
         num_prop=i,
-        type_prop='R',
-        num_type_prop=0,
-        category=category,
         description=description,
         work=work,
         person=person,
@@ -54,7 +58,9 @@ for i in range(1, REPEAT + 1):
     )
     obj.save()
 
+if not NumLastProposal.objects.all().count():
+    NumLastProposal.objects.create(num_last_prop=0)
+
 num_last_proposal = NumLastProposal.objects.get(pk=1)
 num_last_proposal.num_last_prop = REPEAT
 num_last_proposal.save()
-print('%d Proposals salvo com sucesso.' % REPEAT)
