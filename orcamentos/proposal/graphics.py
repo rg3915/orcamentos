121844d0aa238f8d2a086ac0080a16f3fabf9ff0
rg3915 <rg3915@yahoo.com.br>
2016-04-03 00:27:06 -0300

    proposal_per_status_json return get_status_display

--- orcamentos/proposal/graphics.py
+++ orcamentos/proposal/graphics.py
@@ -3,6 +3,7 @@ from django.db.models import Count
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse, JsonResponse
 from .models import Proposal, Contract
+from orcamentos.utils.lists import STATUS_LIST
 
 
 def proposal_per_status_json(request):
@@ -13,10 +14,14 @@ def proposal_per_status_json(request):
         .order_by('status').values('status', 'value')
     '''
     Precisa reescrever o dicionário com os campos do gráfico,
-    que são: 'label' e 'value'.
+    que são: 'label' e 'value'. E ainda retornar o get_status_display.
     '''
-    data = [{'label': x['status'], 'value':x['value']} for x in data]
-    s = json.dumps(list(data), cls=DjangoJSONEncoder)
+    data_list = []
+    for item in data:
+        for choice in STATUS_LIST:
+            if choice[0] == item['status']:
+                data_list.append({'label': choice[1], 'value': item['value']})
+    s = json.dumps(data_list, cls=DjangoJSONEncoder)
     return HttpResponse(s)
 
 
