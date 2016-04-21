c3bca7cb1d36529cd31fbf29bbbf2ef7d6875e1e
rg3915 <rg3915@yahoo.com.br>
2016-04-21 13:07:25 -0300

    Hotfix manage create_entry

--- orcamentos/core/management/commands/create_entry.py
+++ orcamentos/core/management/commands/create_entry.py
@@ -1,33 +1,32 @@
 from django.core.management.base import BaseCommand
 from optparse import make_option
-from orcamentos.core.models import Entry, Category, Work, Person  # , Seller
+from orcamentos.crm.models import Person, Seller
+from orcamentos.proposal.models import Entry, Work
 
 
 class Command(BaseCommand):
+    help = ''' Cria entrada. '''
     option_list = BaseCommand.option_list + (
-        make_option('--priority', default='n',
+        make_option('--priority', default='a3',
                     help="Prioridade (opcional), default é 'normal'."),
-        make_option('--category', default=1,
-                    help="Categoria (opcional), default é 'orçamento'."),
         make_option('--work', help='Nome da obra'),
         make_option('--contact', help='Contato da obra'),
         make_option('--description', default='',
                     help='Descrição (opcional)'),
-        # make_option('--seller', help='Vendedor'),
+        make_option('--seller', help='Vendedor'),
     )
 
-    def handle(self, priority, category, work, contact, description, *args, **kwargs):
+    def handle(self, priority, work, contact, seller, description, *args, **kwargs):
         priority = priority
-        category = Category.objects.get(pk=category)
         work = Work.objects.get(name_work__icontains=work)
         person = Person.objects.get(first_name__icontains=contact)
-        # seller = Seller.objects.get(employee__first_name__icontains=seller)
-        Entry.objects.create(
+        seller = Seller.objects.get(first_name__icontains=seller)
+        entry = Entry(
             priority=priority,
-            category=category,
             work=work,
             person=person,
             description=description,
-            # seller=seller,
+            seller=seller
         )
+        entry.save()
         print('Entrada criada com sucesso.')
