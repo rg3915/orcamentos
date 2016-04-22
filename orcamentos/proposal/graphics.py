9f6fc843080d2abc8764ec3a515d72497e568635
rg3915 <rg3915@yahoo.com.br>
2016-04-22 02:14:40 -0300

    Lista mais performática para gerar o gráfico dos status de proposal

--- orcamentos/proposal/graphics.py
+++ orcamentos/proposal/graphics.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse, JsonResponse
 from .models import Proposal, Contract
 from orcamentos.utils.lists import STATUS_LIST
 
+STATUS_DICT = dict(STATUS_LIST)
+
 
 def proposal_per_status_json(request):
     ''' JSON used to generate the graphic '''
@@ -16,12 +18,9 @@ def proposal_per_status_json(request):
     Precisa reescrever o dicionário com os campos do gráfico,
     que são: 'label' e 'value'. E ainda retornar o get_status_display.
     '''
-    data_list = []
-    for item in data:
-        for choice in STATUS_LIST:
-            if choice[0] == item['status']:
-                data_list.append({'label': choice[1], 'value': item['value']})
-    s = json.dumps(data_list, cls=DjangoJSONEncoder)
+    lista = [{'label': STATUS_DICT[item['status']],
+              'value': item['value']} for item in data]
+    s = json.dumps(lista, cls=DjangoJSONEncoder)
     return HttpResponse(s)
 
 
