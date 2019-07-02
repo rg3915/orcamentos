a6a1d9fb87727fd8e88800dccbd83d7fddf007d0
rg3915 <rg3915@yahoo.com.br>
2019-07-02 20:11:57 -0300

    TDD: teste com url com detail. Close #90

--- orcamentos/crm/tests/test_urls.py
+++ orcamentos/crm/tests/test_urls.py
@@ -2,10 +2,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import resolve_url as r
 from django.test import TestCase
 from django.test.client import Client
-
-
-# path('', c.PersonList.as_view(), name='person_list'),
-# path('<slug>/', c.person_detail, name='person_detail'),
+from django.urls import reverse_lazy as rl
+from orcamentos.crm.models import Person
 
 
 class UrlTest(TestCase):
@@ -15,6 +13,14 @@ class UrlTest(TestCase):
         self.user = User.objects.create_user(**self.credentials)
         self.login = self.client.login(**self.credentials)
         self.resp = self.client.get(r('crm:person_list'))
+        # Cria Person
+        self.person = Person.objects.create(
+            gender='I',
+            first_name='regis',
+            last_name='santos',
+            slug='regis-santos',
+            person_type='p',
+        )
 
     def test_urls_contract(self):
         urls = (
@@ -22,11 +28,21 @@ class UrlTest(TestCase):
                 'url': '/crm/person/',
                 'name': 'person_list',
             },
+            {
+                'url': '/crm/person/{}/'.format(self.person.slug),
+                'name': 'person_detail',
+                'slug': self.person.slug,
+            },
         )
 
         for url in urls:
             with self.subTest():
                 self.r = self.client.get(url['url'])
                 self.assertEqual(200, self.r.status_code)
-                self.r = self.client.get(r('crm:{}'.format(url['name'])))
+                self._url = 'crm:{}'.format(url['name'])
+                if url.get('slug'):
+                    kw = {'slug': self.person.slug}
+                    self.r = self.client.get(rl(self._url, kwargs=kw))
+                else:
+                    self.r = self.client.get(rl(self._url))
                 self.assertEqual(200, self.r.status_code)
