72602859e52be97e4a0a5b695490b38925135650
rg3915 <rg3915@yahoo.com.br>
2019-07-15 15:43:13 -0300

    Testando de cargos existem.

--- orcamentos/crm/tests/test_model_occupation.py
+++ orcamentos/crm/tests/test_model_occupation.py
@@ -8,12 +8,19 @@ class OccupationTest(TestCase):
 
     def setUp(self):
         self.obj = Occupation.objects.create(occupation='Gerente')
+        self.obj2 = Occupation.objects.create(occupation='Diretor')
 
     def test_create(self):
         self.assertTrue(Occupation.objects.exists())
 
     def test_str(self):
         self.assertEqual('Gerente', str(self.obj))
+
+    def test_exists(self):
+        occupations_list = ['Diretor', 'Gerente']
+        occupations = Occupation.objects.filter(
+            occupation__in=occupations_list).values_list('occupation', flat=True).order_by('occupation')
+        self.assertSequenceEqual(occupations, occupations_list)
 
 
 class OccupationTestCase(TestCase):
